5dcdcfcd44bac96caaa5d7fceebb5c098c44ef20
yeong97 <74241604+Yeong97@users.noreply.github.com>
2021-08-16 09:17:12 +0000

    Clear BackJoon 15596

---
+++ BackJoon15596.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Test {
+  long temp;
+  Test() {
+    this.temp=0;
+  }
+  
+  long sum(int[] a) {
+    for(int i=0; i<a.length; i++) {
+      temp = temp + a[i];
+    }
+    return temp;
+  }
+}
+
+class B15596 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+  }
+}
